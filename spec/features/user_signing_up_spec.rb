869c49d2243381f53c69da9e73381a0bac9e5cf2
Patryk Stepien <droznyk@gmail.com>
2018-11-28 15:28:09 +0100

    Add Rubocop

---
+++ spec/features/user_signing_up_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe 'Sign up user' do
+  scenario 'with valid credentials' do
+    visit '/'
+
+    click_link 'Sign up'
+
+    fill_in "Email",	with: "john@example.com" 
+    fill_in "Password",	with: "password" 
+    fill_in "Password confirmation",	with: "password" 
+    click_button "Sign up"
+
+    expect(page).to have_content("You have signed up successfully.")
+    expect(User.last.email).to eq("john@example.com")  
+  end
+
+  scenario 'with invalid credentials' do 
+    before do
+      @john = User.create(email: "john@example.com", password: "password")
+    end
+
+    visit '/'
+    
+    click_link 'Sign up'
+
+    fill_in "Email",	with: ""
+    fill_in "Password",	with: ""
+    fill_in "Password confirmation",	with: "" 
+    click_button "Sign up"
+    
+    expect(page).to have_content("prohibited this user from being saved")
+    expect(User.count).to eq 1
+  end
+end
