15f407e6be74517ae294ba6806feb085f802e859
Patryk Stepien <droznyk@gmail.com>
2018-11-29 12:56:06 +0100

    Refactor false-failure spec

--- spec/features/exercises/creating_exercise_spec.rb
+++ spec/features/exercises/creating_exercise_spec.rb
@@ -11,13 +11,13 @@ RSpec.describe 'Creating exercise' do
 
     click_link 'My Lounge'
     click_link 'New Workout'
+    expect(page).to have_link('Back')
 
     fill_in 'Duration',	with: 70
     fill_in 'Workout details',	with: 'Weight lifting'
     fill_in 'Activity date',	with: '2016-07-26'
     click_button 'Create Exercise'
 
-    expect(page).to have_link('Back')
     expect(page).to have_content('Exercise has been created')
     exercise = Exercise.last
     expect(current_path).to eq(user_exercise_path(@zoe, exercise))
