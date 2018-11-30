09141f29f1d38538920994bc12d4a1e717838ec9
Patryk Stepien <droznyk@gmail.com>
2018-11-30 11:06:13 +0100

    Change hardcoded date to a dynamic date

--- spec/features/exercises/creating_exercise_spec.rb
+++ spec/features/exercises/creating_exercise_spec.rb
@@ -15,7 +15,7 @@ RSpec.describe 'Creating exercise' do
   scenario 'with valid credentials' do
     fill_in 'Duration',	with: 70
     fill_in 'Workout details',	with: 'Weight lifting'
-    fill_in 'Activity date',	with: '2016-07-26'
+    fill_in 'Activity date',	with: 3.days.ago
     click_button 'Create Exercise'
 
     expect(page).to have_content('Exercise has been created')
