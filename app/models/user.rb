5bf086e844316f7eaa60e19a029cb463c1499a29
Patryk Stepien <droznyk@gmail.com>
2018-12-03 15:09:39 +0100

    Set relations between users, friendships and friends

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :exercises
+  has_many :friendships
+  has_many :friends, through: :friendships, class_name: "User"
 
   validates_presence_of :first_name, :last_name
 
@@ -24,5 +26,9 @@ class User < ApplicationRecord
         "%#{names_array[0]}%", "%#{names_array[1]}%",
         "%#{names_array[0]}%", "%#{names_array[1]}%").order(:first_name)
     end
+  end
+
+  def follows_or_same?(new_friend)
+    friendships.map(&:friend).include?(new_friend) || self == new_friend
   end
 end
