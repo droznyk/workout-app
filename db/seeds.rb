88668f79d9a1f4aca8a817d4bd399903ce06437b
Patryk Stepien <droznyk@gmail.com>
2018-12-03 10:55:57 +0100

    Create seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+User.create!(
+  first_name: Faker::Name.first_name,
+  last_name: Faker::Name.last_name,
+  email: 'patryk@example.com',
+  password: 'password'
+)
+
+10.times do
+  User.create!(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email(:first_name),
+    password: 'password'
+  )
+end
+
+puts "#{User.all.count} users created"
+
+User.all.each do |user|
+  10.times do
+    Exercise.create!(
+      duration_in_min: Faker::Number.number(2),
+      workout: Faker::RickAndMorty.quote,
+      workout_date: Faker::Date.between(7.days.ago, Date.today),
+      user_id: user.id
+    )
+  end
+end
+
+puts "#{Exercise.all.count} exercises created"
