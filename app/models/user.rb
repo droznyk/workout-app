ba89dbd96333c1322f689e8c10cb799c005bd058
Patryk Stepien <droznyk@gmail.com>
2018-12-04 12:13:57 +0100

    Associate user with room

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
   has_many :exercises
   has_many :friendships
   has_many :friends, through: :friendships, class_name: "User"
+  has_one :room
 
   validates_presence_of :first_name, :last_name
 
